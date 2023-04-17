c6c5a7368416d6056021e2c90d6fd4fda6d54dbd
ann22120670 <x22120670@student.ncirl.ie>
2023-04-17 14:58:53 +0000

    changes made

--- core/.~c9_invoke_2aitED.py
+++ core/.~c9_invoke_2aitED.py
@@ -10,5 +10,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+ 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-APPLICATION = get_wsgi_application()
+APPLICA = get_wsgi_application()
+ 
+     
+S
