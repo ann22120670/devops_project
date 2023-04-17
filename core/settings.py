fd53782072891c27f9ace929c9ce9e324a196b16
ann22120670 <x22120670@student.ncirl.ie>
2023-04-17 10:38:45 +0000

    changes made

--- core/settings.py
+++ core/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/path/to/your/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
 
