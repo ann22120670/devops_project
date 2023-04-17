4a0b4f34d2caa3905ec702f55e37bc8b75f7070a
ann22120670 <x22120670@student.ncirl.ie>
2023-04-17 18:35:23 +0000

    changes made

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,4 +11,4 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-APPLICATION = get_wsgi_application()
+application = get_wsgi_application()
