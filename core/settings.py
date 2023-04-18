654a3e10dd6f94081cd0a1b2c274ebd721c7720e
ann22120670 <x22120670@student.ncirl.ie>
2023-04-18 22:54:44 +0000

    changes

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 #STATIC_ROOT = ('/path/to/your/static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
