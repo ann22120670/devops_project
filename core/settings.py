e0e02b9196040e42e397554c28edb1f7f10fc0c6
ann22120670 <x22120670@student.ncirl.ie>
2023-04-18 19:34:28 +0000

    dettingschanges

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = ('/path/to/your/static/')
+STATIC_ROOT = ('/path/to/your/static/')
 
 
 CRISPY_TEMPLATE_PACK = ('bootstrap4')
